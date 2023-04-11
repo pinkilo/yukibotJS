352198c299d975d6a963045d0630b3321b5edb15
JonoAugustine <me@jonoaugustine.com>
2023-04-11 08:17:22 -0400

    update ListCommands.ts link

--- src/yuki/commands/ListCommands.ts
+++ src/yuki/commands/ListCommands.ts
@@ -4,6 +4,6 @@ import yt from "../../youtube"
 export default new Command(
   "commands", ["cmds"], 0, 0, 360,
   async () => {
-    await yt.chat.sendMessage("Find a list of commands here https://tinyurl.com/233fu72p")
+    await yt.chat.sendMessage("Find a list of commands here https://tinyurl.com/mt8c5v47")
   },
 )
