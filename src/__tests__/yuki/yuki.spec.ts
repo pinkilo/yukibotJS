ff74b6d6c3b141847b56f9f8134f4da21234119e
JonoAugustine <me@jonoaugustine.com>
2023-05-10 00:35:58 -0400

    tests: add express tests for yuki.spec.ts

--- src/__tests__/yuki/yuki.spec.ts
+++ src/__tests__/yuki/yuki.spec.ts
@@ -5,15 +5,17 @@
 
 import Yuki, { GoogleConfig, YukiConfig } from "../../yuki/Yuki"
 import {
+  AuthEvent,
   BroadcastUpdateEvent,
   Eventbus,
-  EventType,
+  successOf,
   YoutubeWrapper,
 } from "../../internal"
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import winston from "winston"
 import { Credentials } from "google-auth-library"
+import * as supertest from "supertest"
 
 jest.mock("google-auth-library")
 jest.mock("googleapis")
@@ -56,9 +58,6 @@ beforeEach(() => {
   yuki = new Yuki(yukiConfig, youtubeWrapper, tokenLoader, eventbus, logger)
 })
 
-describe("express app", () => {
-  // TODO
-})
 describe("startup", () => {
   describe("startup failure", () => {
     it("should fail if tokenLoader invalid", async () => {
@@ -77,6 +76,16 @@ describe("startup", () => {
       tokenLoader.mockImplementation(async () => tokens)
       expect(await yuki.start()).toBe(true)
       expect(await yuki.start()).toBe(false)
+    })
+    it("should not fetch broadcast", async () => {
+      const spy = jest.spyOn(youtubeWrapper.broadcasts, "fetchBroadcast")
+      await yuki.start()
+      expect(spy).toHaveBeenCalledTimes(0)
+    })
+    it("should should not add broadcast listener for chat watcher", async () => {
+      const listenSpy = jest.spyOn(eventbus, "listen")
+      await yuki.start()
+      expect(listenSpy).toHaveBeenCalledTimes(0)
     })
   })
   describe("startup success", () => {
@@ -105,6 +114,75 @@ describe("startup", () => {
       await eventbus.announce(new BroadcastUpdateEvent(undefined))
       expect(listenSpy).toHaveBeenCalledTimes(1)
       expect(fetchChatSpy).toHaveBeenCalledTimes(1)
+    })
+  })
+})
+
+describe("express app", () => {
+  beforeEach(() => {
+    tokenLoader.mockImplementation(async () => tokens)
+  })
+  it("GET root returns html page", () => {
+    supertest
+      .agent(yuki.express)
+      .get("/")
+      .expect("Content-Type", /html/)
+      .expect(200)
+  })
+  it("GET /auth redirects to generated url", async () => {
+    const redirect = "/redirect"
+    const authUrlSpy = jest
+      .spyOn(youtubeWrapper, "getAuthUrl")
+      .mockName("getAuthUrl")
+      .mockImplementation(() => redirect)
+    await supertest
+      .agent(yuki.express)
+      .get("/auth")
+      .expect("Location", new RegExp(redirect))
+    expect(authUrlSpy).toHaveBeenCalledTimes(1)
+  })
+  describe("GET /callback", () => {
+    it("should redirect home", () => {
+      supertest
+        .agent(yuki.express)
+        .get("/callback")
+        .expect(200)
+        .expect("Location", new RegExp("/"))
+    })
+    describe("with code", () => {
+      const code = "code"
+      const tokens = {}
+      let tokenFetchSpy: jest.SpyInstance
+      beforeEach(() => {
+        tokenFetchSpy = jest
+          .spyOn(youtubeWrapper, "fetchTokensWithCode")
+          .mockName("fetchTokensWithCode")
+          .mockImplementation(async () => successOf(tokens))
+      })
+      it("should call token request", async () => {
+        await supertest.agent(yuki.express).get(`/callback?code=${code}`)
+        expect(tokenFetchSpy).toHaveBeenCalledTimes(1)
+        expect(tokenFetchSpy).toHaveBeenCalledWith(code)
+      })
+      it("should set tokens", async () => {
+        const tokenSetSpy = jest.spyOn(youtubeWrapper, "setTokens")
+        await supertest.agent(yuki.express).get(`/callback?code=${code}`)
+        expect(tokenSetSpy).toHaveBeenCalledTimes(1)
+        expect(tokenSetSpy).toHaveBeenCalledWith(tokens)
+      })
+      it("should announce new tokens", async () => {
+        let event: AuthEvent
+        const announceSpy = jest
+          .spyOn(eventbus, "announce")
+          .mockName("announce")
+          .mockImplementation(async (authEvent: AuthEvent) => {
+            event = authEvent
+          })
+        await supertest.agent(yuki.express).get(`/callback?code=${code}`)
+        expect(announceSpy).toHaveBeenCalledTimes(1)
+        expect(event).toBeInstanceOf(AuthEvent)
+        expect(event.credentials).toEqual(tokens)
+      })
     })
   })
 })
